2ba9bfe80c7f064d9d74effc9912c2e504cf39cc
LeeSomgyul <140570847+LeeSomgyul@users.noreply.github.com>
2024-06-06 17:52:32 +0900

    update

--- Chapter8_텍스트 빈도 분석/8장 코드) 영문 문서 제목의 키워드 분석하기.py
+++ Chapter8_텍스트 빈도 분석/8장 코드) 영문 문서 제목의 키워드 분석하기.py
@@ -1,1 +1,54 @@
-dd
+import pandas as pd
+import glob
+import re
+from functools import reduce
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import STOPWORDS, WordCloud
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+all_files = glob.glob('myCabinetExcelData*.xls')
+
+all_files_data = []
+for file in all_files:
+    data_frame = pd.read_excel(file)
+    all_files_data.append(data_frame)
+all_files_data[0]
+
+all_files_data_concat = pd.concat(all_files_data, axis=0, ignore_index=True)
+
+all_files_data_concat.to_csv('riss_bigdata.csv', encoding = 'utf-8', index = False)
+
+all_title = all_files_data_concat['제목']
+
+stopwords_list = stopwords.words('english')
+lemma = WordNetLemmatizer()
+
+stopwords_list = stopwords.words('english')
+
+words = []
+
+for title in all_title:
+    EnWords = re.sub(r"[^a-zA-Z]+", " ", str(title))
+    EnWordsToken = word_tokenize(EnWords.lower())
+    EnWordsTokenStop = [w for w in EnWordsToken if w not in stopwords_list]
+    EnWordsTokenStopLemma = [lemma.lemmatize(w) for w in EnWordsTokenStop]
+    words.append(EnWordsTokenStopLemma)
+    
+words2 = list(reduce(lambda x, y: x+y, words))
+
+count = Counter(words2)
+
+word_count = dict()
+
+for tag, counts in count.most_common(50):
+    if(len(str(tag))>1):
+        word_count[tag] = counts
+        print("%s : %d" % (tag, counts))
+

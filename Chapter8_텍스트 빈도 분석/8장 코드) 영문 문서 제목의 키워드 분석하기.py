f8b0afdf197b2ac8237d221208fd40dca36135ee
LeeSomgyul <140570847+LeeSomgyul@users.noreply.github.com>
2024-06-07 00:54:44 +0900

    update

--- Chapter8_텍스트 빈도 분석/8장 코드) 영문 문서 제목의 키워드 분석하기.py
+++ Chapter8_텍스트 빈도 분석/8장 코드) 영문 문서 제목의 키워드 분석하기.py
@@ -1,3 +1,4 @@
+#1.데이터 준비
 import pandas as pd
 import glob
 import re
@@ -30,8 +31,6 @@ all_title = all_files_data_concat['제목']
 stopwords_list = stopwords.words('english')
 lemma = WordNetLemmatizer()
 
-stopwords_list = stopwords.words('english')
-
 words = []
 
 for title in all_title:
@@ -43,6 +42,7 @@ for title in all_title:
     
 words2 = list(reduce(lambda x, y: x+y, words))
 
+#2.데이터 탐색 및 분석 모델 구축
 count = Counter(words2)
 
 word_count = dict()
@@ -51,4 +51,31 @@ for tag, counts in count.most_common(50):
     if(len(str(tag))>1):
         word_count[tag] = counts
         print("%s : %d" % (tag, counts))
+        
+sorted_Keys = sorted(word_count, key = word_count.get, reverse=True)
+sorted_Values = sorted(word_count.values(), reverse=True)
+plt.bar(range(len(word_count)), sorted_Values, align='center')
+plt.xticks(range(len(word_count)), list(sorted_Keys), rotation = 85)
+plt.show()
 
+#3.결과 시각화
+all_files_data_concat['doc_count'] = 0
+summary_year = all_files_data_concat.groupby('출판일', as_index = False)['doc_count'].count()
+
+plt.figure(figsize=(12, 5))
+plt.xlabel("year")
+plt.ylabel("doc-count")
+plt.grid(True)
+plt.plot(range(len(summary_year)), summary_year['doc_count'])
+plt.xticks(range(len(summary_year)), [text for text in summary_year['출판일']])
+plt.show()
+
+stopwords = set(STOPWORDS)
+wc = WordCloud(background_color = 'ivory', stopwords = stopwords, width = 800, height = 600)
+cloud = wc.generate_from_frequencies(word_count)
+plt.figure(figsize=(8, 8))
+plt.imshow(cloud)
+plt.axis('off')
+plt.show()
+
+cloud.to_file("riss.bigdata_wordCloud.jpg")
